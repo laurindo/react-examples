d89759b3690d90a1c49a896fce3d33f8e8c0e2b0
Daniel Laurindo <dslaurindo@gmail.com>
2022-04-19 15:17:00 -0300

    Rerun project

---
+++ build/precache-manifest.7aa940b7b76bbdd2335253b6d7081706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2502988559bae328d1f53a6fd38cc847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f486d749bb177690e653",
+    "url": "/static/css/main.a4bbbe49.chunk.css"
+  },
+  {
+    "revision": "3ed5115ceb860d9f631b",
+    "url": "/static/js/2.adb02568.chunk.js"
+  },
+  {
+    "revision": "ca84c80e81f00cf00981527c27a0f8a2",
+    "url": "/static/js/2.adb02568.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f486d749bb177690e653",
+    "url": "/static/js/main.7c917e1a.chunk.js"
+  },
+  {
+    "revision": "b7b56a56fbb63b549511",
+    "url": "/static/js/runtime-main.e3060a99.js"
+  }
+]);
